f6a0c443ec43a4b962489ec2112b4bbb523b8935
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-11-05 22:02:09 +0530

    Login API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
-const { hashPassword } = require("../helpers/authHelper");
+const JWT = require("jsonwebtoken");
+const { hashPassword, comparePassword } = require("../helpers/authHelper");
 const userModel = require("../models/userModel");
 
 const registerController = async (req, res) => {
@@ -45,15 +46,59 @@ const registerController = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    return (
-      res,
-      status(500).send({
-        success: false,
-        message: "Error in Register API",
-        err,
-      })
-    );
+    return res.status(500).send({
+      success: false,
+      message: "Error in Register API",
+      err,
+    });
   }
 };
 
-module.exports = { registerController };
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(500).send({
+        success: false,
+        message: "Please enter valid credentials",
+      });
+    }
+    // find user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(500).send({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+    // match password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.staus(500).send({
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
+    // JWT Token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // undefined password
+    user.password = undefined;
+    res.status(200).send({
+      success: true,
+      message: "Login Successful",
+      user,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Login API",
+      err,
+    });
+  }
+};
+
+module.exports = { registerController, loginController };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { registerController } = require("../controllers/userController");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/userController");
 
 // router
 const router = express.Router();
 
 // routes
+
+// Register || POST
 router.post("/register", registerController);
+
+// Login || POST
+router.post("/login", loginController);
+
 // export
 module.exports = router;
