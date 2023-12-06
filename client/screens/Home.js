c65fefa9f96b59097d5b1456a716ff5652629b1d
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-12-06 22:38:45 +0530

    Header Menu & Logout functionality

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -8,7 +8,6 @@ const Home = () => {
   const [state] = useContext(AuthContext);
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
       <Text>{JSON.stringify(state, null, 4)}</Text>
       <FooterMenu />
     </View>
@@ -21,5 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-between",
+    marginTop: 40,
   },
 });
