29a02f4511d85f7bdca8248bda96a3a076d104a5
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2024-01-20 20:08:47 +0530

    Post Screen

--- client/screens/Post.js
+++ client/screens/Post.js
@@ -1,10 +1,79 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import FooterMenu from "../components/Menus/FooterMenu";
+import FontAwsome5 from "react-native-vector-icons/FontAwesome5";
+import axios from "axios";
 
-const Post = () => {
+const Post = ({ navigation }) => {
+  // local state
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // handle form post DATA
+  const handlePost = async () => {
+    try {
+      setLoading(true);
+      if (!title) {
+        alert("Please add post title.");
+      }
+      if (!description) {
+        alert("Please add post description.");
+      }
+      const { data } = await axios.post("/post/create-post", {
+        title,
+        description,
+      });
+      setLoading(false);
+      alert(data?.message);
+      navigation.navigate("Home");
+    } catch (err) {
+      alert(err.response.data.message || err.message);
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.heading}>Create a Post</Text>
+          <TextInput
+            style={styles.inputBox}
+            placeholder="add post title"
+            placeholderTextColor={"gray"}
+            value={title}
+            onChangeText={(text) => setTitle(text)}
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder="add post description"
+            placeholderTextColor={"gray"}
+            multiline={true}
+            numberOfLines={6}
+            value={description}
+            onChangeText={(desc) => setDescription(desc)}
+          />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <TouchableOpacity style={styles.postBtn} onPress={handlePost}>
+            <Text style={styles.postBtnText}>
+              <FontAwsome5 name="plus-square" size={18} />
+              {"   "}
+              Create Post
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
@@ -19,5 +88,36 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     marginTop: 40,
+  },
+  heading: {
+    textTransform: "uppercase",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  inputBox: {
+    backgroundColor: "#ffffff",
+    textAlignVertical: "top",
+    paddingTop: 10,
+    width: 320,
+    marginTop: 30,
+    fontSize: 16,
+    paddingLeft: 15,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  postBtn: {
+    backgroundColor: "black",
+    width: 300,
+    marginTop: 30,
+    height: 40,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  postBtnText: {
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
